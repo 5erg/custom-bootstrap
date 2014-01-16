90d328e751b11980807864b30b98ca1e037447c2
Sergey Okhotnitski <sergey@drench-design.com>
2014-01-16 11:42:25 +0000

    initial commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="dist/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php include 'head.php';?>
+<html>
+  <body>
+
+  <?php include 'header.php';?>
+  <div id="wrap">
+        <!-- Begin page content -->
+        <div class="container">
+          <div class="page-header">
+            <h1>Let's start</h1>
+          </div>
+          <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
+          <p>Use <a href="../sticky-footer-navbar">the sticky footer with a fixed navbar</a> if need be, too.</p>
+        </div>
+      </div>
+
+  <?php include 'footer.php';?>
+
+  </body>
+</html>
